a375fd3a1e58431c1f3a7289fa44763144011fbb
LU347 <103535661+LU347@users.noreply.github.com>
2024-11-03 21:35:34 -0600

    data setup

--- data.py
+++ data.py
@@ -1,12 +1,24 @@
 import json
 import os
 
+from errors import Errors
+
 #the "database"
 FILE_PATH = "./lib/tasks.json"
 
-def create_json_file(data):
-  if not os.path.exists(FILE_PATH):
+def check_json_file_exists():
+  if os.path.exists(FILE_PATH):
+    return True
+  else:
     with open(FILE_PATH, 'w') as f:
-      json.dump("test", f, indent=4)
+      json.dump({}, f)
 
-create_json_file("test2")
+
+def add_task(new_task):
+  file_exists = check_json_file_exists()
+  
+  try:
+    with open(FILE_PATH, 'a') as f:
+      f.write(new_task)
+    except IOError:
+      sys.exit(Errors.WRITE_FAIL)
--- errors.py
+++ errors.py
@@ -2,3 +2,5 @@ from enum import Enum
 
 class Errors(Enum):
   MISSING_ARG = "Missing arguments"
+  WRITE_FAIL = "Error: Could not update db file"
+
